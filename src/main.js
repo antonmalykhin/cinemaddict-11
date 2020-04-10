3fc5a4919d543ee690c954952b674e286f719cf6
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-11 02:30:48 +0300

    Импортирует и рендерит моки

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import {createShowMoreButtonTemplate} from './components/show-more-btn.js';
 import {createFooterStatisticsTemplate} from './components/footer-statistics.js';
 import {createFilmDetailsTemplate} from './components/film-details.js';
 
+import {generateFilm} from './mock/film.js';
+
 const FILMS_COUNT = 5;
 const EXTRA_FILMS_NUM = 2;
 
@@ -25,8 +27,10 @@ render(siteMainElement, createFilmListTemplate());
 
 const mainFilmsListContainer = siteMainElement.querySelector(`.films-list__container`);
 
+const film = generateFilm();
+
 for (let i = 0; i < FILMS_COUNT; i++) {
-  render(mainFilmsListContainer, createFilmCardTemplate());
+  render(mainFilmsListContainer, createFilmCardTemplate(film));
 }
 
 const filmsBoardElement = siteMainElement.querySelector(`.films`);
@@ -43,8 +47,8 @@ const topRatedFilmsListContainer = topRated.querySelector(`.films-list__containe
 const mostCommentedFilmsListContainer = mostCommented.querySelector(`.films-list__container`);
 
 for (let i = 0; i < EXTRA_FILMS_NUM; i++) {
-  render(topRatedFilmsListContainer, createFilmCardTemplate());
-  render(mostCommentedFilmsListContainer, createFilmCardTemplate());
+  render(topRatedFilmsListContainer, createFilmCardTemplate(film));
+  render(mostCommentedFilmsListContainer, createFilmCardTemplate(film));
 }
 
 const footerStatisticsElement = siteBodyElement.querySelector(`.footer__statistics`);
