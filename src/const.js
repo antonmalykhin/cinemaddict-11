8726ce47a9ff40b4f9b4483e0276c4a4534b5b9a
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-12 00:23:15 +0300

    Добавляет const.js

---
+++ src/const.js
@@ -0,0 +1,26 @@
+const DAY_IN_MILLISECONDS = 86400000;
+
+const Emojis = [
+  `angry`,
+  `puke`,
+  `sleeping`,
+  `smile`
+];
+
+const MonthNames = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+
+export {DAY_IN_MILLISECONDS, Emojis, MonthNames};
