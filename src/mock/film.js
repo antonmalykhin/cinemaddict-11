f7cd6bbdb2613a54f82db186b3e78808865ec66b
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-12 00:38:38 +0300

    Дополняет моки о фильме Импортирует константу MonthNames Добавляет Countries, ProductionTeams, Dates Добавляет функции генерации случайного списка жанров, функцию гененрации случайной даты Форматирует данные

--- src/mock/film.js
+++ src/mock/film.js
@@ -1,4 +1,5 @@
 import {getRandomArrayItem, getRandomIntegerNumber} from '../utils.js';
+import {MonthNames} from '../const.js';
 
 const DESCRIPTION = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 
@@ -11,6 +12,14 @@ const Genres = [
   `Mystery`,
 ];
 
+const Countries = [
+  `USA`,
+  `UK`,
+  `Belgium`,
+  `France`,
+  `Germany`
+];
+
 const FilmImages = {
   'Made For Each Other': `/images/posters/made-for-each-other.png`,
   'Popeye Meets Sinbad': `/images/posters/popeye-meets-sinbad.png`,
@@ -21,9 +30,27 @@ const FilmImages = {
   'The Man With The Golden Arm': `/images/posters/the-man-with-the-golden-arm.jpg`
 };
 
-const Year = {
-  FROM: 1920,
-  TO: 1980
+const ProductionTeams = [{
+  director: `Anthony Mann`,
+  writers: `Anne Wigton, Heinz Herald, Richard Weil`,
+  actors: `Erich von Stroheim, Mary Beth Hughes, Dan Duryea`
+}, {
+  director: `Sam Esmail`,
+  writers: `Sam Esmail, Randolph Leon, Adam Penn`,
+  actors: `Rami Malek, Christian Slater, Carly Chaikin`
+}, {
+  director: `Francis Ford Coppola`,
+  writers: `Mario Puzo, Francis Ford Coppola`,
+  actors: `Marlon Brando, Al Pacino, James Caan`
+}, {
+  director: `Christopher Nolan`,
+  writers: `Jonathan Nolan, Christopher Nolan`,
+  actors: `Matthew McConaughey, Anne Hathaway, Jessica Chastain`
+}];
+
+const Dates = {
+  START: new Date(`01.01.1920`),
+  END: new Date(`01.01.1980`)
 };
 
 const Rating = {
@@ -55,18 +82,38 @@ const getRandomDescription = (count) => {
   return new Array(count).fill(``).map(() => `${getRandomArrayItem(filmDescriptions)}.`).join(``);
 };
 
+const getGenres = (genres) => {
+  const newGenres = [];
+  genres.forEach((it) => {
+    if (Math.random() > 0.5) {
+      newGenres.push(it);
+    }
+  });
+  return newGenres;
+};
+
+const getRandomDate = (start, end) => {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+};
+
 const generateFilm = () => {
   const filmTitle = getRandomArrayItem(Object.keys(FilmImages));
   const filmDuration = getRandomIntegerNumber(Duration.MIN, Duration.MAX);
   const filmDescriptionLength = getRandomIntegerNumber(Description.MIN, Description.MAX);
+  const filmReleaseRaw = getRandomDate(Dates.START, Dates.END);
+  const filmReleaseFormatted = `${filmReleaseRaw.getDate()} ${MonthNames[filmReleaseRaw.getMonth()]} ${filmReleaseRaw.getFullYear()}`;
 
   return {
     poster: FilmImages[filmTitle],
+    ageRate: getRandomIntegerNumber(0, 18),
     title: filmTitle,
+    productionTeam: getRandomArrayItem(ProductionTeams),
+    originalTitle: filmTitle,
     rating: (Math.random() * Rating.MAX).toFixed(Rating.FIXED),
-    year: getRandomIntegerNumber(Year.FROM, Year.TO),
-    duration: formatTime(filmDuration),
-    genre: getRandomArrayItem(Genres),
+    release: filmReleaseFormatted,
+    runtime: formatTime(filmDuration),
+    genres: getGenres(Genres),
+    country: getRandomArrayItem(Countries),
     description: getRandomDescription(filmDescriptionLength),
     comments: getRandomIntegerNumber(Comments.MIN, Comments.MAX)
   };
