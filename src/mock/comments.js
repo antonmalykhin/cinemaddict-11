ad9e1a339f287d4635a8bafdd46e12923b915d8b
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-12 00:26:13 +0300

    Добавляет mock/comment.js

---
+++ src/mock/comments.js
@@ -0,0 +1,33 @@
+import {Emojis} from '../const.js';
+import {getRandomArrayItem, getRandomDateTime, sortMinToMax} from '../utils.js';
+
+const Comments = [
+  `Interesting setting and a good cast`,
+  `Booooooooooring`,
+  `Very very old. Meh`,
+  `Almost two hours? Seriously?`
+];
+
+const Names = [
+  `Lars Horntveth`,
+  `Martin Horntveth`,
+  `Chris Illingworth`,
+  `Nick Blacka`,
+  `Rob Turner`,
+  `Grant Russell`
+];
+
+const generateComment = () => {
+  return {
+    emoji: getRandomArrayItem(Emojis),
+    commentText: getRandomArrayItem(Comments),
+    commentAuthor: getRandomArrayItem(Names),
+    date: getRandomDateTime()
+  };
+};
+
+const generateComments = (count) => {
+  return new Array(count).fill(``).map(generateComment).sort((a, b) => sortMinToMax(a.date, b.date));
+};
+
+export {generateComment, generateComments};
