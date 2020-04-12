08646a097293bb6c372dea61d5b0db334e02a474
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-12 23:24:18 +0300

    Переименовывает в filter.js. Отделяет данные

---
+++ src/components/filter.js
@@ -0,0 +1,23 @@
+const createFilterItem = (filter) => {
+  const {name, count} = filter;
+  return (
+    `<a href="#${name.toLocaleLowerCase()}"  class="main-navigation__item">${name}<span  class="main-navigation__item-count">${count}</span></a>`
+  );
+};
+
+const createFiltersTemplate = (filters) => {
+
+  const filterItems = filters.map((it) => createFilterItem(it)).join(`\n`);
+
+  return (
+    `<nav class="main-navigation">
+      <div class="main-navigation__items">
+        <a href="#all"  class="main-navigation__item   main-navigation__item--active">All movies</ a>
+        ${filterItems}
+      </div>
+      <a href="#stats" class="main-navigation__additional">Stats</a>
+    </nav>`
+  );
+};
+
+export {createFiltersTemplate};
