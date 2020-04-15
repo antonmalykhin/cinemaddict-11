c5096b30d1fced5d901901e1c39aad9eddd04875
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-15 16:04:52 +0300

    Добавляет и экспортирует класс Filters

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,3 +1,5 @@
+import {createElement} from '../utils.js';
+
 const createFilterItem = (filter) => {
   const {name, count} = filter;
   return (
@@ -20,4 +22,26 @@ const createFiltersTemplate = (filters) => {
   );
 };
 
-export {createFiltersTemplate};
+class Filters {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFiltersTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+export default Filters;
