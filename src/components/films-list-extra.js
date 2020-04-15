c8661736152720c749fa906392d708b953e1827a
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-15 15:10:41 +0300

    Добавляет и экспортирует класс FilmsListExtra

--- src/components/films-list-extra.js
+++ src/components/films-list-extra.js
@@ -1,3 +1,5 @@
+import {createElement} from '../utils.js';
+
 const createFilmListExtraTemplate = (extraListTitle) => {
   return (
     `<section class="films-list--extra">
@@ -7,4 +9,27 @@ const createFilmListExtraTemplate = (extraListTitle) => {
   );
 };
 
-export {createFilmListExtraTemplate};
+class FilmsListExtra {
+  constructor(title) {
+    this._title = title;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFilmListExtraTemplate(this._title);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+export default FilmsListExtra;
