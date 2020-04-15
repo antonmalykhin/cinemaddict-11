9fc50fd28b4a42fee7a4a37f837d16596cb77e32
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-15 16:18:25 +0300

    Добавляет и экспортирует класс Sorting

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,3 +1,5 @@
+import {createElement} from '../utils.js';
+
 const createSortTemplate = () => {
   return (
     `<ul class="sort">
@@ -8,4 +10,27 @@ const createSortTemplate = () => {
   );
 };
 
-export {createSortTemplate};
+class Sorting {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSortTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+export default Sorting;
+
