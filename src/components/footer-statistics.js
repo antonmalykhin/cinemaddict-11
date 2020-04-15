263251bc1d864a19563dcafeae1f7e66eb440964
antonmalykhin <a.y.malykhin@gmail.com>
2020-04-15 16:09:02 +0300

    Добавляет и экспортирует класс FooterStatistics

--- src/components/footer-statistics.js
+++ src/components/footer-statistics.js
@@ -1,7 +1,31 @@
+import {createElement} from '../utils.js';
+
 const createFooterStatisticsTemplate = (movies) => {
 
   let formattedMovies = new Intl.NumberFormat(`ru-RU`).format(movies);
   return `<p>${formattedMovies} movies inside</p>`;
 };
 
-export {createFooterStatisticsTemplate};
+class FooterStatistics {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFooterStatisticsTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+export default FooterStatistics;
