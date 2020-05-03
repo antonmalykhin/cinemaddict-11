9704a0cb1ff244d96a49eb4a808039a424adb54f
antonmalykhin <a.y.malykhin@gmail.com>
2020-05-04 02:46:09 +0300

    Добавляет id

--- src/mock/film.js
+++ src/mock/film.js
@@ -99,6 +99,7 @@ const generateFilm = () => {
   const filmReleaseRaw = getRandomDate(Dates.START, Dates.END);
 
   return {
+    id: String(new Date() + Math.random()),
     poster: FilmImages[filmTitle],
     ageRate: getRandomIntegerNumber(0, 18),
     title: filmTitle,
