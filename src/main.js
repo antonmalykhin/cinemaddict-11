d96385686e3ea941e362b1b212ba1f886d96dbe6
antonmalykhin <a.y.malykhin@gmail.com>
2020-05-04 02:56:46 +0300

    Добвляет фильтр и модель

--- src/main.js
+++ src/main.js
@@ -1,29 +1,28 @@
 import UserProfileComponent from './components/user-profile.js';
-import FiltersComponent from './components/filter.js';
 import SortingComponent from './components/sorting';
 import FilmBoardComponent from './components/film-board.js';
 import FooterStatisticsComponent from './components/footer-statistics.js';
 
 import PageController from './controllers/page.js';
+import FilterController from './controllers/filter.js';
+
+import FilmsModel from './models/films.js';
 
 import {generateFilms} from './mock/film.js';
 import {generateUser} from './mock/user.js';
-import {generateFilters} from './mock/filter.js';
 import {generateFilmsInside} from './mock/filmsInside.js';
 
 import {render} from './utils/render.js';
 
-const FILMS_COUNT = 50;
+const FILMS_COUNT = 10;
 
 const siteBodyElement = document.querySelector(`body`);
 const siteHeaderElement = siteBodyElement.querySelector(`.header`);
 const siteMainElement = siteBodyElement.querySelector(`.main`);
 
 const user = generateUser();
-const filters = generateFilters();
 
 render(siteHeaderElement, new UserProfileComponent(user));
-render(siteMainElement, new FiltersComponent(filters));
 
 const sortingComponent = new SortingComponent();
 const filmBoardComponent = new FilmBoardComponent();
@@ -32,9 +31,14 @@ const films = generateFilms(FILMS_COUNT);
 render(siteMainElement, sortingComponent);
 render(siteMainElement, filmBoardComponent);
 
-const pageController = new PageController(filmBoardComponent, sortingComponent);
+const filmsModel = new FilmsModel();
+filmsModel.setFilms(films);
 
-pageController.render(films);
+const filterController = new FilterController(siteMainElement, filmsModel);
+filterController.render();
+
+const pageController = new PageController(filmBoardComponent, sortingComponent, filmsModel);
+pageController.render();
 
 const footerStatisticsElement = siteBodyElement.querySelector(`.footer__statistics`);
 const filmsInside = generateFilmsInside();
