7faaab8b500a88358038217251ad346b5a009099
antonmalykhin <a.y.malykhin@gmail.com>
2020-05-20 15:51:14 +0300

    Добавляет логику отображения рейтинга пользователя

--- src/components/user-profile.js
+++ src/components/user-profile.js
@@ -1,25 +1,53 @@
 import AbstractComponent from './abstract-component';
+import {ProfileRating} from '../const.js';
 
-const createUserProfileTemplate = (user) => {
-  const {userName, avatar} = user;
+const DEFAULT_AVATAR = `bitmap@2x.png`;
+
+const setUserRating = (watchedFilmsCount) => {
+  let ratingValue;
+
+  if (watchedFilmsCount > 0 && watchedFilmsCount <= 10) {
+    ratingValue = ProfileRating.NOVICE;
+  } else if (watchedFilmsCount > 10 && watchedFilmsCount <= 20) {
+    ratingValue = ProfileRating.FAN;
+  } else if (watchedFilmsCount > 20) {
+    ratingValue = ProfileRating.MOVIE_BUFF;
+  } else {
+    ratingValue = ``;
+  }
+
+  return ratingValue;
+};
+
+const getWatchedFilms = (films) => {
+  const initialValue = 0;
+
+  return films.reduce((acc, film) => {
+    return acc + film.isWatched;
+  }, initialValue);
+};
+
+const createUserProfileTemplate = (films) => {
+  const watchedFilms = getWatchedFilms(films);
+  const userRating = setUserRating(watchedFilms);
 
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">${userName}</p>
-      <img class="profile__avatar" src=${avatar} alt="Avatar" width="35" height="35">
+      <p class="profile__rating">${userRating}</p>
+      <img class="profile__avatar" src="images/${DEFAULT_AVATAR}" alt="Avatar" width="35" height="35">
     </section>`
   );
 };
 
 class UserProfile extends AbstractComponent {
-  constructor(user) {
+  constructor(films) {
     super();
 
-    this._user = user;
+    this._films = films;
   }
 
   getTemplate() {
-    return createUserProfileTemplate(this._user);
+    return createUserProfileTemplate(this._films);
   }
 }
 
