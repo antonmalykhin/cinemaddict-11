974376c1fbbe999480b1d1c61680f6a2b739a7f3
antonmalykhin <a.y.malykhin@gmail.com>
2020-05-21 20:33:16 +0300

    Менят лоику устанвки ранга

--- src/components/user-profile.js
+++ src/components/user-profile.js
@@ -1,39 +1,14 @@
 import AbstractComponent from './abstract-component';
-import {ProfileRating} from '../const.js';
+import {getUserRank} from '../utils/common.js';
 
 const DEFAULT_AVATAR = `bitmap@2x.png`;
 
-const setUserRating = (watchedFilmsCount) => {
-  let ratingValue;
-
-  if (watchedFilmsCount > 0 && watchedFilmsCount <= 10) {
-    ratingValue = ProfileRating.NOVICE;
-  } else if (watchedFilmsCount > 10 && watchedFilmsCount <= 20) {
-    ratingValue = ProfileRating.FAN;
-  } else if (watchedFilmsCount > 20) {
-    ratingValue = ProfileRating.MOVIE_BUFF;
-  } else {
-    ratingValue = ``;
-  }
-
-  return ratingValue;
-};
-
-const getWatchedFilms = (films) => {
-  const initialValue = 0;
-
-  return films.reduce((acc, film) => {
-    return acc + film.isWatched;
-  }, initialValue);
-};
-
 const createUserProfileTemplate = (films) => {
-  const watchedFilms = getWatchedFilms(films);
-  const userRating = setUserRating(watchedFilms);
+  const userRank = getUserRank(films);
 
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">${userRating}</p>
+      <p class="profile__rating">${userRank}</p>
       <img class="profile__avatar" src="images/${DEFAULT_AVATAR}" alt="Avatar" width="35" height="35">
     </section>`
   );
