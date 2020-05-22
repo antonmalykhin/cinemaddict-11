15372d275f97dd151c018db94e1f737d53397a71
antonmalykhin <a.y.malykhin@gmail.com>
2020-05-23 02:19:41 +0300

    Реализует работу с сервером
    Импортирует LoadingComponent
    Импортирует API
    Удаляет моки, добавяе возможность работы с сервером
    Добавялет отображение Состояния загрузки

--- src/main.js
+++ src/main.js
@@ -1,58 +1,63 @@
-import UserProfileComponent from './components/user-profile.js';
-import SortingComponent from './components/sorting.js';
+import API from './api.js';
 import FilmBoardComponent from './components/film-board.js';
-import StatisticComponent from './components/statistic.js';
-import FooterStatisticsComponent from './components/footer-statistics.js';
-
-import PageController from './controllers/page.js';
 import FilterController from './controllers/filter.js';
-
 import FilmsModel from './models/films.js';
+import FooterStatisticsComponent from './components/footer-statistics.js';
+import LoadingComponent from './components/loading.js';
+import PageController from './controllers/page.js';
+import SortingComponent from './components/sorting.js';
+import StatisticComponent from './components/statistic.js';
+import UserProfileComponent from './components/user-profile.js';
 
-import {generateFilms} from './mock/film.js';
-import {generateFilmsInside} from './mock/filmsInside.js';
+import {render, remove} from './utils/render.js';
 
-import {render} from './utils/render.js';
-
-const FILMS_COUNT = 10;
+const AUTHORIZATION = `Basic asldksdhf`;
+const END_POINT = `https://11.ecmascript.pages.academy/cinemaddict`;
 
 const siteBodyElement = document.querySelector(`body`);
 const siteHeaderElement = siteBodyElement.querySelector(`.header`);
 const siteMainElement = siteBodyElement.querySelector(`.main`);
 
+const loadingComponent = new LoadingComponent();
 const sortingComponent = new SortingComponent();
 const filmBoardComponent = new FilmBoardComponent();
-const films = generateFilms(FILMS_COUNT);
-
-render(siteHeaderElement, new UserProfileComponent(films));
-render(siteMainElement, sortingComponent);
-render(siteMainElement, filmBoardComponent);
-
+const api = new API(END_POINT, AUTHORIZATION);
 const filmsModel = new FilmsModel();
-filmsModel.setFilms(films);
 
-const filterController = new FilterController(siteMainElement, filmsModel, () => {
-  pageController.show();
-  sortingComponent.show();
-  statisticComponent.hide();
-  statisticComponent.setDefaultFilter();
-});
-filterController.render();
+render(siteMainElement, loadingComponent);
 
-const pageController = new PageController(filmBoardComponent, sortingComponent, filmsModel);
-pageController.render();
+api.getFilms()
+  .then((films) => {
+    filmsModel.setFilms(films);
 
-const statisticComponent = new StatisticComponent(filmsModel);
-render(siteMainElement, statisticComponent);
-statisticComponent.hide();
+    remove(loadingComponent);
+    render(siteHeaderElement, new UserProfileComponent(films));
+    render(siteMainElement, sortingComponent);
+    render(siteMainElement, filmBoardComponent);
 
-const footerStatisticsElement = siteBodyElement.querySelector(`.footer__statistics`);
-const filmsInside = generateFilmsInside();
+    const filterController = new FilterController(siteMainElement, filmsModel, () => {
+      pageController.show();
+      sortingComponent.show();
+      statisticComponent.hide();
+      statisticComponent.setDefaultFilter();
+    });
 
-render(footerStatisticsElement, new FooterStatisticsComponent(filmsInside));
+    filterController.render();
 
-filterController.setOnStatisticClick(() => {
-  statisticComponent.rerender();
-  pageController.hide();
-  statisticComponent.show();
-});
+    const pageController = new PageController(filmBoardComponent, sortingComponent, filmsModel, api);
+    pageController.render();
+
+    const statisticComponent = new StatisticComponent(filmsModel);
+    render(siteMainElement, statisticComponent);
+    statisticComponent.hide();
+
+    const footerStatisticsElement = siteBodyElement.querySelector(`.footer__statistics`);
+
+    render(footerStatisticsElement, new FooterStatisticsComponent(filmsModel));
+
+    filterController.setOnStatisticClick(() => {
+      statisticComponent.rerender();
+      pageController.hide();
+      statisticComponent.show();
+    });
+  });
