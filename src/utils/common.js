3f3355a2ff148d4239ef81424e657d68fed5d595
antonmalykhin <a.y.malykhin@gmail.com>
2020-05-28 21:56:02 +0300

    Удаляет неиспользуемые функции. Фиксит ошибки

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,24 +1,8 @@
 import moment from 'moment';
 import {ProfileRank} from '../const.js';
 
-export const getRandomArrayItem = (array) => {
-  return array[Math.floor(Math.random() * array.length)];
-};
-
-export const getRandomIntegerNumber = (min, max) => {
-  return min + Math.floor(Math.random() * (max - min));
-};
-
 export const toTitleCase = (word) => {
   return word[0].toUpperCase() + word.slice(1);
-};
-
-export const getRandomDateTime = () => {
-  const targetDateTime = new Date();
-  targetDateTime.setDate(targetDateTime.getDate() - getRandomIntegerNumber(0, 10));
-  targetDateTime.setHours(targetDateTime.getHours() - getRandomIntegerNumber(0, 24));
-  targetDateTime.setMinutes(targetDateTime.getMinutes() - getRandomIntegerNumber(0, 60));
-  return targetDateTime;
 };
 
 export const formatDate = (date) => {
@@ -47,15 +31,6 @@ export const formatCommentDateTime = (date, timeFrame) => {
   return moment().diff(moment(date), `days`) < timeFrame ? moment(date).fromNow() : moment(date).format(`YYYY/MM/DD hh:mm`);
 };
 
-export const sortMinToMax = (a, b) => {
-  if (a > b) {
-    return 1;
-  } else if (a < b) {
-    return -1;
-  }
-  return 0;
-};
-
 export const getUserRank = (watchedFilmsCount) => {
   if (watchedFilmsCount > 0 && watchedFilmsCount <= 10) {
     return ProfileRank.NOVICE;
@@ -63,7 +38,7 @@ export const getUserRank = (watchedFilmsCount) => {
     return ProfileRank.FAN;
   } else if (watchedFilmsCount > 20) {
     return ProfileRank.MOVIE_BUFF;
-  } else {
-    return ``;
   }
+
+  return ``;
 };
