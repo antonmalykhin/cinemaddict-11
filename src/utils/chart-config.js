7cafca30dfa693401fa62cd7e05733cc02841ddc
antonmalykhin <a.y.malykhin@gmail.com>
2020-05-28 21:50:35 +0300

    Добавляет модуль с конфигом чарта
    В модуле функция, которая принимаетсписки плагинов, тайтлов и значений,
    возвращает обхект конфига чарта

---
+++ src/utils/chart-config.js
@@ -0,0 +1,58 @@
+export const configChart = (plugins, titles, values) => {
+  return {
+    plugins: [plugins],
+    type: `horizontalBar`,
+    data: {
+      labels: titles,
+      datasets: [{
+        data: values,
+        backgroundColor: `#ffe800`,
+        hoverBackgroundColor: `#ffe800`,
+        anchor: `start`,
+        barThickness: 24
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 20
+          },
+          color: `#ffffff`,
+          anchor: `start`,
+          align: `start`,
+          offset: 40,
+        }
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#ffffff`,
+            padding: 100,
+            fontSize: 20
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false
+      }
+    }
+  };
+};
